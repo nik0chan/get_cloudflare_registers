12db8918512a6d0851199d08f1ddfa38771ab282
Steam Deck User <nik0chan@hotmail.com>
2023-11-11 17:23:33 +0100

    Added HTML and XML generation

--- README.md
+++ README.md
@@ -3,16 +3,17 @@ Python script to retrieve all A and CNAME registers  from a cloudflare domain
 
 Usage: 
 
- python3 /root/scripts/cloudflare/get_cloudflare_registers.py <zone_key> [debug level]
+ python3 /root/scripts/cloudflare/get_cloudflare_registers.py <zone_key> [debug level] [output_file]
 
 Parameters: 
 
  zone_key: Mandatory - Specifies your Cloudflare API key with premissions to read information from your account. 
  debug: Optional - Specifies debug level 0,1 or 2 
+ output_file: Specifies ouput file name/path, it can create and XLS or HTML file depending on file extension you specify 
 
 Example 
  
-  python3 /root/scripts/cloudflare/get_cloudflare_registers.py wcFE2f4.2fñ429ef8cl32fj34klw99ñw 2
+  python3 /root/scripts/cloudflare/get_cloudflare_registers.py wcFE2f4.2fñ429ef8cl32fj34klw99ñw 2 /tmp/output.html 
 
 Another example: 
 
@@ -23,4 +24,4 @@ zone2,token2
 zone3,token3 
 ... 
 
-awk -F, '{print $2}' tokens_zonas.csv | xargs -i python3 get_cloudflare_registers.py {} 0
+awk -F, '{print $2}' tokens_zones.csv | xargs -i python3 get_cloudflare_registers.py {} 1
--- get_cloudflare_registers.py
+++ get_cloudflare_registers.py
@@ -1,9 +1,14 @@
 # get_cloudflare_registers.py
-# NMS - 26/08/2023
+# NMS - 11/11/2023
 # Description:
 # Given an Cloudflare token identification bearer returns info for A/CNAME registers, proxy status and type of register of all domains configured for this zone
+#
+# Requirements:
+#
+# python3 -m pip install openpyxl
+#
 # Syntax: (Parameter Bearer key is mandatory):
-# python3 get_cloudflare_registers.py <Bearer key> [debug level] 
+# python3 get_cloudflare_registers.py <Bearer key>
 #
 # Generating Cloudlfare API authentication Bearer key process:
 #
@@ -15,6 +20,7 @@
 import requests
 import json
 import sys
+import openpyxl
 
 def get_dns_records(zone_id, record_type, key, debug=0):
     # Returns all registers on a zone, you must specify zone_id, record type "A/CNAME" and authentication Bearer
@@ -48,28 +54,97 @@ def get_zone_identifiers(bearer_token, debug=0):
         'Authorization': f'Bearer {bearer_token}',
         'Content-Type': 'application/json'
     }
-
     if debug>1:
         print("Making request to https://api.cloudflare.com/client/v4/zones/?per_page=400")
-
+        print("With headers: {headers}")  
     response = requests.get('https://api.cloudflare.com/client/v4/zones/?per_page=400', headers=headers)
     data = json.loads(response.text)
     zone_ids = [zone['id'] for zone in data['result']]
     return zone_ids
 
-def main(debug=0):
+def write_to_html(records, output_file):
+    with open(output_file, 'w') as f:
+        f.write('<html><head>')
+        f.write('<title>DNS Records</title>')
+        f.write('<link rel="stylesheet" type="text/css" href="styles.css">')
+        f.write('</head><body>')
+        f.write('<table class="styled-table">')
+        f.write('<tr><th>Type</th><th>Name</th><th>Content</th><th>Proxy Enabled</th></tr>')
+        for record in records:
+            proxy_enabled = record["proxied"]
+            if proxy_enabled == 0:
+               f.write(f'<tr class="red-row"><td>{record["type"]}</td><td>{record["name"]}</td><td>{record["content"]}</td><td>{record["proxied"]}</td></tr>')
+            else:
+               f.write(f'<tr><td>{record["type"]}</td><td>{record["name"]}</td><td>{record["content"]}</td><td>{record["proxied"]}</td></tr>')
+        f.write('</table>')
+        f.write('</body></html>')
+
+def write_to_excel(records, output_file):
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = 'DNS Records'
+
+    ws.append(['Type', 'Name', 'Content', 'Proxy Enabled'])
+
+    for record in records:
+        ws.append([record["type"], record["name"], record["content"], record["proxied"]])
+
+    wb.save(output_file)
+
+def main(debug, output_file='output'):
    if debug:
         print("{:10}{:50}{:120}{:10}".format("Type", "Name", "Content", "Proxy_Enabled"))
-   for zone in get_zone_identifiers(sys.argv[1], debug):
-      for record in (get_dns_records(zone,"CNAME",sys.argv[1],debug)):
-            #print(record["type"],record["name"],record["content"],record["proxied"])
-          print(f"{record['type']:10}{record['name']:50}{record['content']:120}{record['proxied']:10}")
-      for record in (get_dns_records(zone,"A",sys.argv[1], debug)):
-          #  print(record["type"],record["name"],record["content"],record["proxied"])
-          print(f"{record['type']:10}{record['name']:50}{record['content']:120}{record['proxied']:10}")
+
+   tokens = sys.argv[1]
+   all_records = []
+
+   if ',' in tokens: # Multiple CSV zone tokens
+     for token in tokens.split(','):
+         for zone in get_zone_identifiers(token, debug):
+             for record in (get_dns_records(zone,"A",token,debug)):
+                 if "domainkey" not in record['content']:
+                    print(f"{record['type']:10}{record['name']:50}{record['content']:120}{record['proxied']:10}")
+                    all_records.append(record)
+             for record in (get_dns_records(zone,"CNAME",token, debug)):
+                 if "domainkey" not in record['content']:
+                    print(f"{record['type']:10}{record['name']:50}{record['content']:120}{record['proxied']:10}")
+                    all_records.append(record)
+   else: # Single token
+     for zone in get_zone_identifiers(tokens, debug):
+        for record in (get_dns_records(zone,"A",tokens,debug)):
+            if "domainkey" not in record['content']:
+               if debug >= 1:
+                  print(f"{record['type']:10}{record['name']:50}{record['content']:120}{record['proxied']:10}")
+               all_records.append(record)
+        for record in (get_dns_records(zone,"CNAME",tokens, debug)):
+            if "domainkey" not in record['content']:
+               if debug >= 1:
+                  print(f"{record['type']:10}{record['name']:50}{record['content']:120}{record['proxied']:10}")
+               all_records.append(record)
+
+   if all_records and output_file != 'none': # File output
+      if output_file.lower().endswith('.xls') or output_file.lower().endswith('.xlsx'):
+         write_to_excel(all_records, output_file)
+      elif output_file.lower().endswith('.htm') or output_file.lower().endswith('.html'):
+         write_to_html(all_records, output_file)
+      else:
+         print("Format not supported on filename. Please use .xls, .xlsx or .htm .html format for output.")
+         print("Example: ")
+         print("python3 get_cloudflare_registers.py 4jO0k3G-T0fMLl60aBVZnUT2ad33a [debug] [output_file.xls] ")
+
 if __name__ == '__main__':
     try:
        debug = int(sys.argv[2])
+       if len(sys.argv) > 2:
+          output_file = sys.argv[3]
+       else:
+          output_file = none
     except IndexError:
-       debug = 0
-    main(debug)
+       debug = 1
+       output_file = 'none'
+    except ValueError:
+       print("ERROR - Unexpected input")
+       print("Syntax: ")
+       print("python3 get_cloudflare_registers.py 4jO0k3G-T0fMLl60aBVZnUT2ad33a [debug] [output_file.xls]")
+       exit()
+    main(debug,output_file)
